26078ada4af2da6cb647a0326924acb351dc6cc7
Sameed <87655480+UnderCoverTable@users.noreply.github.com>
2023-02-01 16:28:14 +0500

    Testing with the adafruit library

---
+++ Chad PCA9685 ver/RobotArm_TestingWithAdafruitLibrary_PCA9685/RobotArm_TestingWithAdafruitLibrary_PCA9685.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();   // Initiates library.
+
+#define SERVOMIN  10  // Minimum pulse length count out of 4096.
+#define SERVOMAX  380 // Maximum pulse length count out of 4096.
+
+int servoNo = 0; // Defines a counter for servos.
+
+
+void setup() 
+{
+  Serial.begin(9600);       // Starts serial connecton at 9600 baud rate.
+  
+  pwm.begin();         // Sends PWM signals.
+  pwm.setPWMFreq(60);  // Makes servos run at 60 Hz rate.
+  delay(20);
+}
+
+
+void loop() 
+{   
+  
+    pwm.setPWM(servoNo, 2, 450);                            // to maximum pulse length then to minimum pulse length.
+  
+
+
+} 
